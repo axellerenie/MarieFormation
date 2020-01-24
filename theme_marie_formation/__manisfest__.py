9894bc84a271642c6e5114cbac734c206b2ba3a1
Blouk <nicolas@blouk.com>
2020-01-24 15:55:28 +0100

    [START]

---
+++ theme_marie_formation/__manisfest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Marie Formation',
+    'version': '1.0',
+    'category': 'theme',
+    'sequence': 2,
+    'summary': 'Marie Formation',
+    'description': """
+Marie Formation
+""",
+    'author': 'Odoo SA',
+    'depends': [
+
+    ],
+    'data': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
